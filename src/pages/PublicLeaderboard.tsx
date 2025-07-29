18c2ab27c0a40ebc9044a334106b4768ac9f17a3
crownthesound <info@crownthesound.com>
2025-07-28 19:48:16 -0700

    Updated PublicLeaderboard.tsx

--- src/pages/PublicLeaderboard.tsx
+++ src/pages/PublicLeaderboard.tsx
@@ -478,12 +478,6 @@ export function PublicLeaderboard() {
             <div className="block sm:hidden w-full h-full">
               {/* Compact Mobile Layout */}
               <div className="flex flex-col h-full justify-center px-2">
-                {/* Crown Logo - Smaller and centered */}
-                <div className="flex justify-center mb-3">
-                  <div className="w-12 h-12 bg-gradient-to-br from-orange-400 to-yellow-500 rounded-full flex items-center justify-center border-2 border-white shadow-xl">
-                    <Crown className="h-6 w-6 text-white" />
-                  </div>
-                </div>
                 
                 {/* Prize Podium - Horizontal compact layout */}
                 <div className="relative max-w-7xl mx-auto w-full mb-4 mt-12">
